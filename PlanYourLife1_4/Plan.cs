01e1a4712a1b698be94dbdcdd6f4e3342efe4b44
darina.dulja <darina.dulja@gmail.com>
2019-07-25 17:57:56 +0300

    Добавьте файлы проекта.

---
+++ PlanYourLife1_4/Plan.cs
@@ -0,0 +1,112 @@
+﻿using System;
+using MySql.Data.MySqlClient;
+
+namespace PlanYourLife1_4
+{
+    class Plan
+    {
+        public int id { set; get; }
+        public DateTime date { set; get; }
+        public string text { set; get; }
+        public bool implementation { set; get; }
+        /// <summary>
+        /// Конструктор класу
+        /// </summary>
+        /// <param name="id"></param>
+        /// <param name="date"></param>
+        /// <param name="text"></param>
+        /// <param name="implementation"></param>
+        public Plan(int id, DateTime date, string text, bool implementation)
+        {
+            this.id = id;
+            this.date = date;
+            this.text = text;
+            this.implementation = implementation;
+        }
+        
+        /// <summary>
+        /// Конструктор, що створює новий екземпляр класу, надаючи йому наступний порядковий номер і додає даний план в бд
+        /// </summary>
+        /// <param name="date"></param>
+        /// <param name="text"></param>
+        /// <param name="implementation"></param>
+        public Plan(DateTime date, string text, bool implementation)
+        {
+            MySqlConnection connection = Plans.Connection();
+            connection.Open();
+            string commandStr = "SELECT MAX(Id) FROM plans";
+            MySqlCommand command = new MySqlCommand(commandStr, connection);
+            MySqlDataReader reader = command.ExecuteReader();
+            int Id=1;
+            while (reader.Read())
+            {
+                Id += Convert.ToInt32(reader[0]);
+            }
+            
+            connection.Close();
+            this.id = Id;
+            this.date = date;
+            this.text = text;
+            this.implementation = implementation;
+            this.AddToDB();
+        }
+        public void AddToDB()
+        {
+            string DateStr = this.date.ToString("yyyy-MM-dd hh:mm:ss");
+            string commandStr = "INSERT INTO plans (Id, Date, Text, Implementation) VALUES " +
+                "('"+this.id+"', '"+DateStr+"', '"+this.text+"', '"+this.implementation+"')";
+            MySqlConnection connection = Plans.Connection();
+            connection.Open();
+            MySqlCommand command = new MySqlCommand(commandStr, connection);
+            command.ExecuteNonQuery();
+            connection.Close();
+        }
+        public void DeleteFromDB()
+        {
+            string commandStr = "DELETE FROM plans WHERE Id='" + this.id + "'";
+            MySqlConnection connection = Plans.Connection();
+            connection.Open();
+            MySqlCommand command = new MySqlCommand(commandStr, connection);
+            command.ExecuteNonQuery();
+            connection.Close();
+        }
+        public void UpdateDate(DateTime newDate)
+        {
+            string DateStr = newDate.ToString("yyyy-MM-dd hh:mm:ss");
+            string commandStr="UPDATE plans SET Date='"+DateStr+ "'" +
+                " WHERE Id='" + this.id + "'";
+            MySqlConnection connection = Plans.Connection();
+            connection.Open();
+            MySqlCommand command = new MySqlCommand(commandStr, connection);
+            command.ExecuteNonQuery();
+            connection.Close();
+        }
+      
+        public void UpdateText(string Text)
+        {
+            string commandStr= "UPDATE plans SET Text='" + Text + "' WHERE Id='" + this.id + "'";
+            MySqlConnection connection = Plans.Connection();
+            connection.Open();
+            MySqlCommand command = new MySqlCommand(commandStr, connection);
+            command.ExecuteNonQuery();
+            connection.Close();
+        }
+        public void ChangeImplamentation()
+        {
+            bool newImplemetation = !this.implementation;
+            string commandStr = "UPDATE plans SET Implementation='" + newImplemetation + "' WHERE Id='" + this.id + "'";
+            MySqlConnection connection = Plans.Connection();
+            connection.Open();
+            MySqlCommand command = new MySqlCommand(commandStr, connection);
+            command.ExecuteNonQuery();
+            connection.Close();
+        }
+        /// <summary>
+        /// Виводить на консоль об'єкт класу 
+        /// </summary>
+        public void ShowPlan()
+        {
+            Console.WriteLine(id + "\t" + date + "\t" + text + "\t" + implementation);
+        }
+    }
+}
---
+++ PlanYourLife1_4/Plans.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using MySql.Data.MySqlClient;
+using MySql.Data.Types;
+
+namespace PlanYourLife1_4
+{
+    class Plans
+    {
+      public List<Plan> plans { set; get; }
+        public Plans()
+        {
+            MySqlConnection connection = Connection();
+            connection.Open();
+            plans = new List<Plan>();
+            string commandStr = "select* from plans";
+            MySqlCommand command = new MySqlCommand(commandStr, connection);
+            MySqlDataReader reader = command.ExecuteReader();
+            while (reader.Read())
+            {
+                Plan ob = new Plan(Convert.ToInt32(reader[0]), Convert.ToDateTime(reader[1].ToString()), reader[2].ToString(), Convert.ToBoolean(reader[3]));
+                plans.Add(ob);
+            }
+            connection.Close();
+        }
+      
+        public void ShowPlans()
+        {
+           foreach(Plan plan in plans)
+            {
+                plan.ShowPlan();
+            }
+        }
+       public static MySqlConnection Connection()
+        {
+            string connectionString = "server=127.0.0.1; user=root; database=planyourlife; password=;convert zero datetime=True;charset='utf8';";
+            MySqlConnection connection = new MySqlConnection(connectionString);
+            return connection;
+        }
+    }
+}
